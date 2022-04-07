7c61966dcd04c1850beebf4506b58e63ee338101
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-04-08 00:27:10 +0530

    done without events

--- src/components/Invities.jsx
+++ src/components/Invities.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import gh1 from "../assets/goh.png";
+import gh2 from "../assets/goh2.png";
+import chief from "../assets/cg.png";
+import board from "../assets/board.jpg";
 
 const Invities = () => {
   return (
@@ -10,12 +14,12 @@ const Invities = () => {
               <div class="flex flex-wrap justify-center text-center mb-24">
                 <div class="w-full lg:w-6/12 px-4">
                   <h1 class="text-white text-4xl font-bold mb-8">
-                    Meet the Team
+                    Meet the previous guest's we've invited
                   </h1>
 
                   <p class="text-white text-lg font-light">
-                    With over 100 years of combined experience, we've got a
-                    well-seasoned team at the helm.
+                    With over many years of combined experience, we've got a
+                    well-seasoned list of guests at the helm.
                   </p>
                 </div>
               </div>
@@ -26,17 +30,18 @@ const Invities = () => {
                     <a href="#" class="mx-auto">
                       <img
                         class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                        src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=80"
+                        // src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=80"
+                        src={gh1}
                       />
                     </a>
 
                     <div class="text-center mt-6">
                       <h1 class="text-white text-xl font-bold mb-1">
-                        Tranter Jaskulski
+                        Shri.A. GOPAL RAO
                       </h1>
 
                       <div class="text-white font-light mb-2">
-                        Founder & Specialist
+                        Chairman & Managing Director, TSNPDCL
                       </div>
 
                       <div
@@ -52,17 +57,18 @@ const Invities = () => {
                     <a href="#" class="mx-auto">
                       <img
                         class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                        src="https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80"
+                        // src="https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80"
+                        src={chief}
                       />
                     </a>
 
                     <div class="text-center mt-6">
                       <h1 class="text-white text-xl font-bold mb-1">
-                        Denice Jagna
+                        Shri.D. PRABHAKR RAO
                       </h1>
 
                       <div class="text-white font-light mb-2">
-                        Tired & M. Specialist
+                        Chairman & Managing Director, TSGENCO & TRANSCO
                       </div>
 
                       <div
@@ -78,16 +84,19 @@ const Invities = () => {
                     <a href="#" class="mx-auto">
                       <img
                         class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                        src="https://images.unsplash.com/photo-1634193295627-1cdddf751ebf?fit=clamp&w=400&h=400&q=80"
+                        // src="https://images.unsplash.com/photo-1634193295627-1cdddf751ebf?fit=clamp&w=400&h=400&q=80"
+                        src={gh2}
                       />
                     </a>
 
                     <div class="text-center mt-6">
                       <h1 class="text-white text-xl font-bold mb-1">
-                        Kenji Milton
+                        Shri.G. RAGHUMA REDDY
                       </h1>
 
-                      <div class="text-white font-light mb-2">Team Memeber</div>
+                      <div class="text-white font-light mb-2">
+                        Chairman & Managing Director, TSSPDCL
+                      </div>
 
                       <div
                         class="flex items-center justify-center opacity-50 hover:opacity-100
@@ -102,17 +111,18 @@ const Invities = () => {
                     <a href="#" class="mx-auto">
                       <img
                         class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                        src="https://images.unsplash.com/photo-1635003913011-95971abba560?fit=clamp&w=400&h=400&q=80"
+                        // src="https://images.unsplash.com/photo-1635003913011-95971abba560?fit=clamp&w=400&h=400&q=80"
+                        src={board}
                       />
                     </a>
 
                     <div class="text-center mt-6">
                       <h1 class="text-white text-xl font-bold mb-1">
-                        Doesn't matter
+                        manny DIRECTOR'S
                       </h1>
 
                       <div class="text-white font-light mb-2">
-                        Will be fired
+                        from across state electricity board.
                       </div>
 
                       <div
--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import winner from "../assets/winner.gif";
 const TimeLine = () => {
   return (
     // <body
@@ -10,19 +10,19 @@ const TimeLine = () => {
       <div className="bg-black text-white py-8">
         <div className="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
           <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
-            <p className="ml-2 text-yellow-300 uppercase tracking-loose">
+            <p className="ml-2 text-blue-500 font-bold uppercase tracking-loose">
               Working Process
             </p>
             <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">
               Working Process of Fest
             </p>
             <p className="text-sm md:text-base text-gray-50 mb-4">
-              Here’s your guide to the tech fest 2021 process. Go through all
+              Here’s your guide to the techsonance 2022 process. Go through all
               the steps to know the exact process of the fest.
             </p>
             <a
               href="#"
-              className="bg-transparent mr-auto hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
+              className="bg-transparent mr-auto hover:bg-blue-300 text-blue-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-blue-300 hover:border-transparent"
             >
               Explore Now
             </a>
@@ -31,26 +31,26 @@ const TimeLine = () => {
             <div className="container mx-auto w-full h-full">
               <div className="relative wrap overflow-hidden p-10 h-full">
                 <div
-                  className="border-2-2 border-yellow-555 absolute h-full border"
+                  className="border-2-2 border-blue-555 absolute h-full border"
                   style={{
                     right: "50%",
-                    border: "2px solid #FFC100",
+                    border: "2px solid #0cf2e1",
                     borderRadius: "1%",
                   }}
                 />
                 <div
-                  className="border-2-2 border-yellow-555 absolute h-full border"
+                  className="border-2-2 border-blue-555 absolute h-full border"
                   style={{
                     left: "50%",
-                    border: "2px solid #FFC100",
+                    border: "2px solid #0cf2e1",
                     borderRadius: "1%",
                   }}
                 />
                 <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                   <div className="order-1 w-5/12" />
                   <div className="order-1 w-5/12 px-1 py-4 text-right">
-                    <p className="mb-3 text-base text-yellow-300">
-                      1-6 May, 2021
+                    <p className="mb-3 text-base text-blue-300">
+                      1-25 April, 2022
                     </p>
                     <h4 className="mb-3 font-bold text-lg md:text-2xl">
                       Registration
@@ -65,8 +65,8 @@ const TimeLine = () => {
                 <div className="mb-8 flex justify-between items-center w-full right-timeline">
                   <div className="order-1 w-5/12" />
                   <div className="order-1  w-5/12 px-1 py-4 text-left">
-                    <p className="mb-3 text-base text-yellow-300">
-                      6-9 May, 2021
+                    <p className="mb-3 text-base text-blue-300">
+                      20-29 April, 2022
                     </p>
                     <h4 className="mb-3 font-bold text-lg md:text-2xl">
                       Participation
@@ -81,40 +81,38 @@ const TimeLine = () => {
                 <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                   <div className="order-1 w-5/12" />
                   <div className="order-1 w-5/12 px-1 py-4 text-right">
-                    <p className="mb-3 text-base text-yellow-300">
-                      {" "}
-                      10 May, 2021
+                    <p className="mb-3 text-base text-blue-300">
+                      29 April, 2022
                     </p>
                     <h4 className="mb-3 font-bold text-lg md:text-2xl">
                       Result Declaration
                     </h4>
                     <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
                       The ultimate genius will be revealed by our judging panel
-                      on 10th May, 2021 and the resukts will be announced on the
-                      whatsapp groups and will be mailed to you.
+                      on 29th April, 2022 and the results will be announced.
                     </p>
                   </div>
                 </div>
                 <div className="mb-8 flex justify-between items-center w-full right-timeline">
                   <div className="order-1 w-5/12" />
                   <div className="order-1  w-5/12 px-1 py-4">
-                    <p className="mb-3 text-base text-yellow-300">
-                      12 May, 2021
+                    <p className="mb-3 text-base text-blue-300">
+                      30 April, 2022
                     </p>
                     <h4 className="mb-3 font-bold  text-lg md:text-2xl text-left">
                       Prize Distribution
                     </h4>
                     <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                      The winners will be contacted by our team for their
-                      addresses and the winning goodies will be sent at their
-                      addresses.
+                      The Prizes for all the event winners will be distributed
+                      by the chief guest in the e-classroom.
                     </p>
                   </div>
                 </div>
               </div>
               <img
-                className="mx-auto -mt-36 md:-mt-36"
-                src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png"
+                className="mx-auto -mt-36 md:-mt-36 "
+                src={winner}
+                // src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png"
               />
             </div>
           </div>
