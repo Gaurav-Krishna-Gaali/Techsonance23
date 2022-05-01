2d0095d06ab5d07c3214c2b3207e784eb51bf5f7
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-05-01 13:02:38 +0530

    Images Added

--- src/components/Guests.jsx
+++ src/components/Guests.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Try.css";
 import SBI from "../assets/imp/logo.png";
+import redco from "../assets/imp/tsredco.png";
+import tt1 from "../assets/imp/tt1.png";
 
 const Guests = () => {
   return (
@@ -35,28 +37,72 @@ const Guests = () => {
         <div class="card 2">
           <div
             class="card_image"
-            onClick={() =>
-              window.open(
-                "http://uceou.edu/electrical/electrical.html",
-                "_blank"
-              )
-            }
+            onClick={() => window.open("https://www.onlinesbi.com/", "_blank")}
           >
             <img src={SBI} className="p-10" alt="SBI" />
+          </div>
+          <div class="card_title text-xl title-white hover:text-blue-700 "></div>
+        </div>
+        <div class="card 3">
+          <div
+            class="card_image"
+            onClick={() =>
+              window.open("https://tsredco.telangana.gov.in/", "_blank")
+            }
+          >
+            <img src={redco} className="p-10" alt="SBI" />
           </div>
           <div class="card_title text-xl title-white hover:text-blue-700 ">
             {/* <p className="text-xl pt-20 hover:text-blue-700 underline">SBI</p> */}
           </div>
         </div>
-
-        {/* <div class="card 3">
-        <div class="card_image">
-          <img src="https://th.bing.com/th/id/R.46a71fab15fea178dc9c6f6baf69167d?rik=ulNpIqcetz3e6w&riu=http%3a%2f%2f25.media.tumblr.com%2ftumblr_m9ogt65l771r0rljlo1_1280.jpg&ehk=Xv69ZN6eRC%2bNz0WX3TDIHjSfsCY%2bHRdOBgJGZjzoHNg%3d&risl=&pid=ImgRaw&r=0" />
+        <div class="card 4">
+          <div
+            class="card_image"
+            onClick={() =>
+              window.open("https://telanganatourism.gov.in/", "_blank")
+            }
+          >
+            <img src={tt1} className="p-10" alt="SBI" />
+          </div>
+          <div class="card_title text-xl title-white hover:text-blue-700 ">
+            {/* <p className="text-xl pt-20 hover:text-blue-700 underline">SBI</p> */}
+          </div>
         </div>
-        <div class="card_title">
-          <p>Card Title</p>
-        </div>
-      </div> */}
+        {/* <div class="card 5">
+          <div
+            class="card_image"
+            onClick={() =>
+              window.open("https://telanganatourism.gov.in/", "_blank")
+            }
+          >
+            <img src={tt1} className="p-10" alt="SBI" />
+          </div>
+          <div class="card_title text-xl title-white hover:text-blue-700 ">
+          </div>
+        </div> */}
+        {/* <div class="card 6">
+          <div
+            class="card_image"
+            onClick={() =>
+              window.open("https://telanganatourism.gov.in/", "_blank")
+            }
+          >
+            <img src={tt1} className="p-10" alt="SBI" />
+          </div>
+          <div class="card_title text-xl title-white hover:text-blue-700 "></div>
+        </div> */}
+        {/* <div class="card 7">
+          <div
+            class="card_image"
+            onClick={() =>
+              window.open("https://telanganatourism.gov.in/", "_blank")
+            }
+          >
+            <img src={tt1} className="p-10" alt="SBI" />
+          </div>
+          <div class="card_title text-xl title-white hover:text-blue-700 "></div>
+        </div> */}
 
         {/* <div class="card 4">
         <div class="card_image">
